dc16e84d38808f1428da1fb8b589355b5e269d0d
setiawan7490 <setiawan5858585@gmail.com>
2024-12-17 10:33:28 +0700

    Soal4

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # stream_rendi
 
-Soal 1
-Soal 2
 Soal 3
     -fungsi dari yield* adalah menyederhanakan delegasi iterable atau stream.
 
     -maksudnya adalah untuk mendelegasikan semua elemen dari generator lain kedalam generator saat ini
+
 Soal 4
+    !(<assets/img/1.png>)
+    !(<assets/img/2.png>)
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'stream.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,9 +28,34 @@ class StreamHomePage extends StatefulWidget {
 }
 
 class _StreamHomePageState extends State<StreamHomePage> {
+  Color bgColor = Colors.blueGrey;
+  late ColorStream colorStream;
+
+  void changeColor() async {
+    await for (var eventColor in colorStream.getColors()) {
+      setState(() {
+        bgColor = eventColor;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    colorStream = ColorStream();
+    changeColor();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stream'),
+        backgroundColor: Colors.indigo,
+      ),
+      body: Container(
+        decoration: BoxDecoration(color: bgColor),
+      ),
+    );
   }
 }
-
